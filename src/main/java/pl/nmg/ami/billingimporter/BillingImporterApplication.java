a32b4dca955ca5631d30dfdb838be65eced1b61b
Michał <olszewskimichal@outlook.com>
2017-11-26 17:48:01 +0100

    Inicjalizacja projektu

---
+++ src/main/java/pl/nmg/ami/billingimporter/BillingImporterApplication.java
@@ -0,0 +1,12 @@
+package pl.nmg.ami.billingimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingImporterApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/nmg/ami/billingimporter/BillingImporterApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.nmg.ami.billingimporter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BillingImporterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
